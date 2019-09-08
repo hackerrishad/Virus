7a356b49dd7ae88892996eedd35e3bc16d151be6
sheikhrishad <51270384+hackerrishad@users.noreply.github.com>
2019-09-08 22:04:28 +0600

    Add files via upload

---
+++ Virus.bat
@@ -0,0 +1,23 @@
+
+@ECHO off
+echo hello
+pause
+goto a
+:a
+color 0a
+echo %random% you are hacked %random%
+color 19
+echo %random% you are hacked %random%
+color 2b
+echo %random% you are hacked %random%
+color 3B
+echo %random% you are hacked %random%
+color 4C
+echo %random% you are hacked %random%
+color 5D
+echo %random% you are hacked %random%
+color 6E
+echo %random% you are hacked %random%
+color 7F
+echo %random% you are hacked %random%
+goto a
